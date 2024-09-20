7c726b918914aa40b1ffe996d06b72af2a91c7b5
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:21:30 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -7,9 +7,9 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/fzfport.git"]]])
+                        userRemoteConfigs: [[url: "https://github.com/zopencommunity/fzfport.git"]]])
         }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/fzfport.git'), string(name: 'PORT_DESCRIPTION', value: 'A command-line fuzzy finder' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/fzfport.git'), string(name: 'PORT_DESCRIPTION', value: 'A command-line fuzzy finder' )]
   }
 }
