5dc41da45b890756925dd3018f8449bf1c7f47b7
Igor Todorovski <39890068+IgorTodorovskiIBM@users.noreply.github.com>
2025-03-13 16:07:08 -0400

    Update cicd.groovy - make it run on v3r1 only

--- cicd.groovy
+++ cicd.groovy
@@ -10,6 +10,7 @@ node('linux')
                         userRemoteConfigs: [[url: "https://github.com/zopencommunity/fzfport.git"]]])
         }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/fzfport.git'), string(name: 'PORT_DESCRIPTION', value: 'A command-line fuzzy finder' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/fzfport.git'), string(name: 'PORT_DESCRIPTION', value: 'A command-line fuzzy finder' ), string(name: 'NODE_LABEL', value: "v3r1" ) ]
+  }
   }
 }
